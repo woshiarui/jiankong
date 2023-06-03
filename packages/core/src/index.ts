8b8ee545c5f5d0e66e74ca190e52daabff1fcacd
Zheng20010712 <Zheng20010712@163.com>
2023-06-03 20:37:29 +0800

    feat:lint

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,8 +5,8 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-06-03 18:55:54
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-06-03 18:57:03
+ * @LastEditTime: 2023-06-03 20:36:27
  */
 export function core() {
-    console.log('i am core111')
+    console.log('i am core1111')
 }
