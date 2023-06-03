1c33f8e5305c778a90e6c4d11ce979378ca36769
Zheng20010712 <Zheng20010712@163.com>
2023-06-03 20:47:32 +0800

    fe:test husky

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,8 +5,8 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-06-03 18:55:54
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-06-03 20:36:27
+ * @LastEditTime: 2023-06-03 20:46:54
  */
 export function core() {
-    console.log('i am core1111')
+    console.log('i am core11111')
 }
