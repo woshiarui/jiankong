a75b2e35875316390596e1dccf05515e3d16b53f
Zheng20010712 <Zheng20010712@163.com>
2023-06-03 19:34:55 +0800

    feat: init

---
+++ packages/core/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhengXiaoRui
+ * @email: zheng20010712@163.com
+ * @Date: 2023-06-03 18:55:54
+ * @LastEditors: ZhengXiaoRui
+ * @LastEditTime: 2023-06-03 18:57:03
+ */
+export function core() {
+    console.log('i am core')
+}
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ZhengXiaoRui
+ * @email: zheng20010712@163.com
+ * @Date: 2023-06-03 18:55:54
+ * @LastEditors: ZhengXiaoRui
+ * @LastEditTime: 2023-06-03 18:57:03
+ */
+export function utils() {
+    console.log('i am utils')
+}
