fdddb92274e28411de1c3489e046380095687c2d
Zheng20010712 <Zheng20010712@163.com>
2023-07-03 23:53:22 +0800

    feat: fetchReplace

--- packages/core/src/core/replace.ts
+++ packages/core/src/core/replace.ts
@@ -10,7 +10,7 @@ import { notify, subscribeEvent } from "./subscribe";
  * @email: zheng20010712@163.com
  * @Date: 2023-06-04 16:12:53
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-07-02 20:14:03
+ * @LastEditTime: 2023-07-03 23:52:49
  */
 
 function replace(type: EVENT_TYPES) {
@@ -67,6 +67,52 @@ function xhrReplace(): void {
                 return
             })
             originalSend.apply(this, args)
+        }
+    })
+}
+
+function fetchReplace(): void {
+    if (!('fetch' in _global)) return
+    replaceAop(_global, EVENT_TYPES.FETCH, (originFetch) => {
+        return function (url, config: Partial<Request> = {}) {
+            const startTime = getTimeStamp()
+            const method = (config && config.method) || 'GET'
+            let fetchData = {
+                type: HTTPTYPE.FETCH,
+                method,
+                requestData: config && config.body,
+                url,
+                response: '',
+            }
+
+            //headers
+            const headers = new Headers(config.headers || {})
+            // TODO: why？
+            Object.assign(headers, {
+                setRequestHeader: headers.set
+            })
+            config = Object.assign({}, config, headers)
+            return originFetch.apply(_global, [url, config]).then((res) => {
+                const tempRes = res.clone()
+                const endTime = getTimeStamp()
+                fetchData = Object.assign({}, fetchData, {
+                    elapsedTime: endTime - startTime,
+                    Status: tempRes.status,
+                    time: startTime
+                })
+                tempRes.text().then(() => {
+                    notify(EVENT_TYPES.FETCH, fetchData)
+                })
+            }).catch((err) => {
+                const endTime = getTimeStamp()
+                fetchData = Object.assign({}, fetchData, {
+                    elapsedTime: endTime - startTime,
+                    Status: 0,
+                    time: startTime
+                })
+                notify(EVENT_TYPES.FETCH, fetchData)
+                throw err; //抛出错误，等着被捕获
+            })
         }
     })
 }
