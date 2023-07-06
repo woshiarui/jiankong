d33b922812da441403881fecdfe5dac4dc98d827
chenjiarong <chenjiarong@leapstack.cn>
2023-07-06 15:34:18 +0800

    feat:初始化vue3项目，新增网络请求按钮

---
+++ packages/test/vue/src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+import axios from 'axios'
+async function handleClick() {
+    const data = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    console.log(data)
+  }
+
+</script>
+
+<template>
+  <el-button @click="handleClick">发送网络请求</el-button>
+</template>
