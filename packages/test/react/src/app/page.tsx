ca8b34f5154ae0a85382288051ae604fe1cdbf40
chenjiarong <chenjiarong@leapstack.cn>
2023-07-07 10:39:49 +0800

    feat(test):React和Vue新增自定义网络请求

--- packages/test/react/src/app/page.tsx
+++ packages/test/react/src/app/page.tsx
@@ -1,13 +1,35 @@
 'use client'
 
-import { Button } from 'antd'
+import { Button, Form, Input } from 'antd'
 import axios from 'axios'
+import { MouseEventHandler } from 'react'
 export default function Home() {
-  async function handleClick() {
-    const data = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  const sendRequest = async (url: string) => {
+    const data = await axios.get(url)
     console.log(data)
   }
+
+  function onSubmit(values: any) {
+    sendRequest(values.url)
+  }
+
   return (
-    <Button onClick={handleClick}>发送网络请求</Button>
+    <>
+      <Button onClick={()=> sendRequest('https://jsonplaceholder.typicode.com/todos/1')}>发送网络请求</Button>
+      <Form
+        name="basic"
+        wrapperCol={{ span: 24 }}
+        onFinish={onSubmit}
+      >
+        <Form.Item label="网络请求" name="url" wrapperCol={{ span: 24 }}>
+          <Input placeholder="输入url"/>
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 8 }}>
+          <Button type="primary" htmlType="submit">
+            发送
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
--- packages/test/vue/src/components/HelloWorld.vue
+++ packages/test/vue/src/components/HelloWorld.vue
@@ -1,12 +1,30 @@
 <script setup>
 import axios from 'axios'
-async function handleClick() {
-    const data = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    console.log(data)
-  }
+import { reactive } from 'vue'
+
+const sendRequest = async (url) => {
+  const data = await axios.get(url)
+  console.log(data)
+}
+
+const form = reactive({
+  url: '',
+})
+
+function onSubmit() {
+  sendRequest(form.url)
+}
 
 </script>
 
 <template>
-  <el-button @click="handleClick">发送网络请求</el-button>
+  <el-button @click="sendRequest('https://jsonplaceholder.typicode.com/todos/1')">发送网络请求</el-button>
+  <el-form :model="form">
+    <el-form-item label="网络请求">
+      <el-input v-model="form.url" placeholder="输入url" clearable />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">发送</el-button>
+    </el-form-item>
+  </el-form>
 </template>
