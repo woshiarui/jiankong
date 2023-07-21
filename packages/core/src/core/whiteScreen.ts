8b59b6f34061d6700d2da7a7100fba50ce116232
Zheng20010712 <Zheng20010712@163.com>
2023-07-21 21:38:12 +0800

    fix: 注释

--- packages/core/src/core/whiteScreen.ts
+++ packages/core/src/core/whiteScreen.ts
@@ -5,13 +5,37 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-07-10 22:47:01
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-07-17 23:07:26
+ * @LastEditTime: 2023-07-18 20:42:04
  */
 import { STATUS_CODE } from "@rmonitor/common";
 import { Callback } from "@rmonitor/types";
 import { InitOptions } from "@rmonitor/types/src/core/option";
 import { _global, _support } from "@rmonitor/utils";
-
+/**
+ * 什么情况下会出现白屏？
+ * 1.代码执行错误：spa框架里面如果出现子组件报错，其实就导致页面白屏。React ErrorBoundary
+ * 2.资源加载错误：HTML、CSS、JS、bundle.js
+ * 
+ * 迭代：诊断为什么出现白屏
+ * 
+ * 骨架屏 === 白屏
+ * 
+ * 监测白屏：
+ * 1.白色图片——算法——页面比较   =>  白屏   缺点：骨架屏不行、实现比较困难
+ * 2.DOM数量———特别少  =>  缺点：不准确
+ * 3.截图（DOM树、canvas）——DOM分析  =>  canvas 缺点：性能不太好
+ * 4.截图——native（微信、支付宝）  =>  缺点：应用局限
+ * 5.采样对比——白屏
+ * 
+ * 第一个方案选择（每个方案的优缺点）
+ * 第二个我们实现方案——采样
+ * 改进：白屏修复机制
+ * 
+ * 两个参数：什么时候白屏上报5s？什么时候白屏修复机制的截止时间10s
+ * 0s的时候dom元素 cache = []
+ * @param callback 
+ * @param param1 
+ */
 export function openWhiteScreen(callback: Callback, { skeletonProject, whiteBoxElements }: InitOptions) {
     let _whiteLoopNum = 0
     const _skeletonInitList: any[] = [] // 存储初次采样点
@@ -108,6 +132,11 @@ export function openWhiteScreen(callback: Callback, { skeletonProject, whiteBoxE
         }, 1000)
     }
 
+    /**
+     * let num = 2
+     * TODO: 如果一段时间内都没有执行，强制执行两遍
+     * @returns 
+     */
     function idleCallback() {
         //轮询超时
         if (+new Date() > overTime) return
