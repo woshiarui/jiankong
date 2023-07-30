3153c97b05262403832353fee9fe0f48f8bb0624
Zheng20010712 <Zheng20010712@163.com>
2023-07-30 17:14:58 +0800

    fix: whiteScreen bugfix and readme

--- packages/core/CHANGELOG.md
+++ packages/core/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @rmonitor/core
 
+## 3.0.0
+
+### Major Changes
+
+- 0.0.1
+
 ## 2.1.1
 
 ### Patch Changes
--- packages/core/src/core/whiteScreen.ts
+++ packages/core/src/core/whiteScreen.ts
@@ -5,41 +5,25 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-07-10 22:47:01
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-07-22 11:42:17
+ * @LastEditTime: 2023-07-30 16:05:30
  */
 import { STATUS_CODE } from "@rmonitor/common";
 import { Callback, InitOptions } from "@rmonitor/types";
-import { _global, _support } from "@rmonitor/utils";
+import { _global, _support, generateUUID } from "@rmonitor/utils";
 /**
  * 什么情况下会出现白屏？
  * 1.代码执行错误：spa框架里面如果出现子组件报错，其实就导致页面白屏。React ErrorBoundary
  * 2.资源加载错误：HTML、CSS、JS、bundle.js
- * 
- * 迭代：诊断为什么出现白屏
- * 
- * 骨架屏 === 白屏
- * 
- * 监测白屏：
- * 1.白色图片——算法——页面比较   =>  白屏   缺点：骨架屏不行、实现比较困难
- * 2.DOM数量———特别少  =>  缺点：不准确
- * 3.截图（DOM树、canvas）——DOM分析  =>  canvas 缺点：性能不太好
- * 4.截图——native（微信、支付宝）  =>  缺点：应用局限
- * 5.采样对比——白屏
- * 
- * 第一个方案选择（每个方案的优缺点）
- * 第二个我们实现方案——采样
- * 改进：白屏修复机制
- * 
- * 两个参数：什么时候白屏上报5s？什么时候白屏修复机制的截止时间10s
- * 0s的时候dom元素 cache = []
  * @param callback 
  * @param param1 
  */
 export function openWhiteScreen(callback: Callback, { isHasLoading, whiteBoxElements }: InitOptions) {
+    let uuid = generateUUID()
     let _whiteLoopNum = 0
     const _skeletonInitList: any[] = [] // 存储初次采样点
     let _skeletonNowList: any[] = [] //当前采样点
     let overTime = +new Date() + 10000 //待配置
+    let isWhite = true
 
     //如果项目有骨架屏
     if (isHasLoading) {
@@ -96,11 +80,10 @@ export function openWhiteScreen(callback: Callback, { isHasLoading, whiteBoxElem
                 //第一次不比较
                 if (!_whiteLoopNum) return openWhiteLoop()
                 //比较前后dom是否一致
-                if (_skeletonNowList.join() == _skeletonInitList.join()) {
-                    return callback({
-                        status: STATUS_CODE.ERROR
-                    })
-                }
+                return callback({
+                    uuid,
+                    status: _skeletonNowList.join() == _skeletonInitList.join() ? STATUS_CODE.OK : STATUS_CODE.ERROR
+                })
             }
             if (_support._loopTimer) {
                 clearTimeout(_support._loopTimer)
@@ -112,8 +95,8 @@ export function openWhiteScreen(callback: Callback, { isHasLoading, whiteBoxElem
                 openWhiteLoop()
             }
         }
-
         callback({
+            uuid,
             status: emptyPoints == 17 ? STATUS_CODE.ERROR : STATUS_CODE.OK
         })
     }
@@ -132,8 +115,7 @@ export function openWhiteScreen(callback: Callback, { isHasLoading, whiteBoxElem
     }
 
     /**
-     * let num = 2
-     * TODO: 如果一段时间内都没有执行，强制执行两遍
+     * 
      * @returns 
      */
     function idleCallback() {
@@ -145,6 +127,8 @@ export function openWhiteScreen(callback: Callback, { isHasLoading, whiteBoxElem
                 if (deadline.timeRemaining() > 0) {
                     sampling()
                 }
+            }, {
+                timeout: 1000 //后续改为配置，对性能有影响
             })
         } else {
             sampling()
